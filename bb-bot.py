0e2d25ffe29d3241d0a173f04d7b8ad2561bcf70
Mohammad Tanvir <tanvir0102@gmail.com>
2021-08-01 10:27:41 +0530

    Create bb-bot.py

---
+++ bb-bot.py
@@ -0,0 +1,10 @@
+from os import system
+msg=input("Enter the target(bing.com): ")
+system("curl -s 'https://crt.sh/?q={}' | grep '<TD>' | grep {} | cut -d '>' -f2 | cut -d '<' -f1 | sort -u | sed '/^*/d' | tee -a {}.txt".format(msg,msg,msg))
+system("curl -s 'https://rapiddns.io/subdomain/{}#result' | grep '<td><a' | cut -d '\"' -f 2 | grep http | cut -d '/' -f3 | sed '/^*/d' | tee -a {}.txt".format(msg,msg))
+system("curl -s 'https://jldc.me/anubis/subdomains/{}' | grep -Po '((http|https):\/\/)?(([\w.-])\.([\w])\.([A-z]))\w+' | cut -d '/' -f3 | sort -u | tee -a {}.txt".format(msg,msg))
+system("sort {}.txt | uniq | sed '/^*/d' | tee -a {}".format(msg,msg))
+system(f"rm -r {msg}.txt")
+system(f"cat {msg} | httpx -o {msg}")
+system("nuclei -update-templates")
+system(f"nuclei -l {msg} -t nuclei-templates/ -severity critical,high,medium,low | notify")
